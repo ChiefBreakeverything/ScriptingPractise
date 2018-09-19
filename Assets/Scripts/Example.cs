35a95b4f19a4909f1227488516c0233277293df3
Lucas - Surface <lucas.hehir@gmail.com>
2018-09-19 19:35:22 +1000

    We have an example!
    It's so bad

---
+++ Assets/Scripts/Example.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example : MonoBehaviour {
+
+	public bool trueOrFalse;
+	public float decimalNumber = 5.0f;
+	public float speed = 1.0f;
+	public int aWholeNumber = 20;
+	public string firstName = "Lucas";
+	public int playerHealth;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Hello world");
+	}
+
+	
+
+	// Update is called once per frame
+	void Update () {
+		transform.eulerAngles += new Vector3(0, speed, 0);
+	}
+}
