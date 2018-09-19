d413159320479404d1df76101898396064aa2574
Lucas - Surface <lucas.hehir@gmail.com>
2018-09-19 20:29:29 +1000

    Added Jump function and a transparency slider
    It's so good

--- Assets/Scripts/Example.cs
+++ Assets/Scripts/Example.cs
@@ -11,6 +11,8 @@ public class Example : MonoBehaviour {
 	public string firstName = "Lucas";
 	public int playerHealth;
 
+	public Color materialColor;
+
 	// Use this for initialization
 	void Start () {
 		Debug.Log("Hello world");
@@ -20,6 +22,26 @@ public class Example : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		transform.eulerAngles += new Vector3(0, speed, 0);
+		// We need this to spin so it will hit the player
+		//transform.eulerAngles += new Vector3(0, speed, 0);
+
+		// We want the cube to be hard to hit
+		//transform.position += new Vector3(speed, 0, 0);
+
+		//transform.Rotate(new Vector3(0, 0, speed));
+		//transform.Rotate(new Vector3(speed, 0, 0));
+
+		//transform.Rotate(Vector3.up, Space.World);
+
+		//GetComponent<MeshRenderer>().material.color = materialColor;
+	}
+
+	public void Jump(float force) {
+		GetComponent<Rigidbody>().AddForce(0, force, 0);
+	} 
+
+	public void Fade(float sliderValue) {
+		materialColor.a = sliderValue;
+		GetComponent<MeshRenderer>().material.color = materialColor;
 	}
 }
